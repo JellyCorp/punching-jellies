03cef22a706ea735294d34ff692e4fe5b240d52b
b0nj0urm0nsieur <thornermateo2010@yahoo.fr>
2023-12-12 16:42:59 +0100

    braining

--- src/backend/Main.py
+++ src/backend/Main.py
@@ -3,7 +3,7 @@ from time import sleep
 import threading
 
 from .Map import Map
-from .Player import Player, Actions
+from .Player import Player
 
 
 class Main(threading.Thread):
@@ -24,15 +24,18 @@ class Main(threading.Thread):
         return count <= 1 or self.map.get_time() >= self.map.max_t
 
     def run(self):
+        i = 0
         while not self.is_over():
             for player in self.players:
-                player.next_action = choice(list(Actions))
+                player.decide()
             for player in self.players:
                 player.update()
-            # sleep(25*1e-2)
+            sleep(1 / 60)
+            i += 1
+        print(i)
 
 
 if __name__ == "__main__":
-    main = Main(start_positions=[(368, 20), (368, 648)])
+    main = Main(start_positions=[(368, 20), (368, 648)], grid_dim=(500, 700), max_t=1)
     main.start()
     print(main.players[0].history)
--- src/backend/Player.py
+++ src/backend/Player.py
@@ -1,20 +1,30 @@
 from enum import Enum
 from time import sleep
+import sys
+import os
 
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)  # TODO shitty solution, needs to be cleared
+
+from models.FullyConnected import FullyConnected
 from .Position import Position
 from .Map import Map
 
 
 class Actions(Enum):
+    none = None
     move_right = "move_right"
     move_left = "move_left"
-    none = None
 
 
 class Player:
     count = 0
 
-    def __init__(self, position: Position, map: Map):
+    def __init__(self, position: Position, map: Map, brain: FullyConnected = None):
+        self.brain = (
+            brain if brain is not None else FullyConnected([3, 4, len(Actions)])
+        )
         self.id = Player.count
         self.hit_points = 100
         self.stamina = 10
@@ -25,17 +35,28 @@ class Player:
         self.size = (32, 32)
         Player.count += 1
 
-    def __repr__(self) -> str:
-        return f"{self.id} -> {self.position}\n"
+        self.checkpoint_step = 0
 
-    def move_right(self, dist=10):
+    def __repr__(self) -> str:
+        return f"{self.id} -> {self.position} -> {self.checkpoint_step}\n"
+
+    def decide(self):
+        thoughts = self.brain(
+            [self.position.x, self.size[1], self.map.checkpoints[self.checkpoint_step]]
+        )
+        print(
+            f"{self.id} - {thoughts} - {self.checkpoint_step} - {abs(self.position.x - self.map.checkpoints[self.checkpoint_step])}"
+        )
+        self.next_action = list(Actions)[thoughts.index(max(thoughts))]
+
+    def move_right(self, dist=1):
         start_position = self.position
         self._move((0, 1), dist)
         self.history.append(
             (self.map.get_time(), Actions.move_right, start_position, self.position)
         )
 
-    def move_left(self, dist=20):
+    def move_left(self, dist=1):
         start_position = self.position
         self._move((0, -1), dist)
         self.history.append(
@@ -46,7 +67,7 @@ class Player:
         for _ in range(dist):
             if self.map.is_valid(self.position + Position(vect), self.size):
                 self.position += Position(vect)
-            sleep(1 / 60)
+            # sleep(1 / 60)
 
     def update(self):
         assert isinstance(self.next_action, Actions)
@@ -57,8 +78,19 @@ class Player:
                 self.move_left()
         self.next_action = Actions.none
 
+        if (
+            self.position.x <= self.map.checkpoints[self.checkpoint_step]
+            and self.map.checkpoints[self.checkpoint_step]
+            <= self.position.x + self.size[1]
+            and self.checkpoint_step < len(self.map.checkpoints)
+        ):
+            self.checkpoint_step += 1
+
 
 if __name__ == "__main__":
-    p = Player((5, 20), Map((100, 100), 5))
+    p = Player(position=(5, 20), map=Map((500, 700), 5))
+    print(p.position)
+    p.decide()
     p.update()
-    p.move_left()
+    print(p.position)
+    print(len(Actions))
